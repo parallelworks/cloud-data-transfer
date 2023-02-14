861959c6b2928c432b1c75bd9949a0bd1533968e
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud-00064.c.modular-magpie-167320.internal>
2023-02-14 18:16:08 +0000

    Added streamlined timing with SECONDS

--- test_aws_cli.sh
+++ test_aws_cli.sh
@@ -10,10 +10,10 @@
 echo Select file
 #=============================================
 # Big object - 6.3GB
-test_obj="s3://cloud-data-benchmarks/ETOPO1_Ice_g_gmt4.csv"
+#test_obj="s3://cloud-data-benchmarks/ETOPO1_Ice_g_gmt4.csv"
 
 # Small object - 100MB
-#test_obj="s3://cloud-data-benchmarks/ETOPO1_Ice_g_gmt4_tenthdeg.csv"
+test_obj="s3://cloud-data-benchmarks/ETOPO1_Ice_g_gmt4_tenthdeg.csv"
 
 bn=$(basename $test_obj)
 bucket=$(dirname $test_obj)
@@ -29,7 +29,11 @@ echo Object size: $size_mb
 #=============================================
 echo Transfer object
 #=============================================
-time aws s3 cp $test_obj $bn > /dev/null
+SECONDS=0
+aws s3 cp $test_obj $bn > /dev/null
+dl_s=$SECONDS
+dl_r=$(echo $size_mb $dl_s | awk '{print $1/$2}' )
+echo Download took $dl_s seconds at $dl_r MB/s
 
 #=============================================
 echo Rename with move
@@ -40,12 +44,20 @@ mv $bn $tmp_name
 #=============================================
 echo Copy back up to bucket
 #=============================================
-time aws s3 cp ${tmp_name} ${bucket}/${tmp_name} > /dev/null
+SECONDS=0
+aws s3 cp ${tmp_name} ${bucket}/${tmp_name} > /dev/null
+ul_s=$SECONDS
+ul_r=$(echo $size_mb $ul_s | awk '{print $1/$2}' )
+echo Upload took $ul_s seconds at $ul_r MB/s
 
 #=============================================
 echo Delete copy from bucket
 #=============================================
-time aws s3 rm ${bucket}/${tmp_name} > /dev/null
+SECONDS=0
+aws s3 rm ${bucket}/${tmp_name} > /dev/null
+rm_s=$SECONDS
+rm_r=$(echo $size_mb $rm_s | awk '{print $1/$2}' )
+echo Delete took $rm_s seconds at $rm_r MB/s
 
 #=============================================
 # Clean up
