7d46ed29563e9d8fe3974638fbcd340f3950edd2
StefanGary <sfgary@parallelworks.com>
2023-02-24 14:01:49 +0000

    Test only uses aws sync or (parallel) aws cp, reduced stdout

--- test_aws_cli.sh
+++ test_aws_cli.sh
@@ -9,9 +9,10 @@
 #=============================================
 #echo Create a bunch of files
 #=============================================
-num_files=$1
-file_size=$2
-size_unit=$3
+aws_sync=$1
+num_files=$2
+file_size=$3
+size_unit=$4
 s3_bucket="s3://cloud-data-benchmarks/"
 
 sync_dir=tmp_${RANDOM}
@@ -26,7 +27,7 @@ done
 wait
 
 dir_size_mb=$(du -BMB $sync_dir | tail -1 | awk -FM '{print $1}')
-echo Testing AWS CLI with $sync_dir with size $dir_size_mb MB
+#echo Testing AWS CLI with $sync_dir with size $dir_size_mb MB
 
 dir_file_list="${sync_dir}/*.bin"
 #echo Contents in $sync_dir 
@@ -41,85 +42,83 @@ dir_file_list="${sync_dir}/*.bin"
 #size_mb=$(aws s3 ls $test_obj | awk '{print $3/1.0e6}')
 #echo Object size: $size_mb
 
-#=============================================
-#echo Upload objects with AWS sync
-#=============================================
-# Force seconds timer to always start at minimum
-# value of 1 in case operation takes less than a
-# second (b/c resulting rate will be div by 0).
-SECONDS=1
-aws s3 sync $sync_dir ${s3_bucket}${sync_dir} > /dev/null
-ul_s=$SECONDS
-ul_r=$(echo $dir_size_mb $ul_s | awk '{print $1/$2}' )
-echo Serial upload took $ul_s s at $ul_r MB/s
+if [ ${aws_sync} = "sync" ]
+then
 
-#=============================================
-#echo Delete objects in bucket
-#=============================================
-#SECONDS=1
-#aws s3 rm ${s3_bucket}${sync_dir} > /dev/null
-#rm_s=$SECONDS
-#rm_r=$(echo $dir_size_mb $rm_s | awk '{print $1/$2}' )
-#echo Bucket object delete took $rm_s seconds at $rm_r MB/s
+	#=============================================
+	#echo Upload objects with AWS sync
+	#=============================================
+	# Force seconds timer to always start at minimum
+	# value of 1 in case operation takes less than a
+	# second (b/c resulting rate will be div by 0).
+	SECONDS=1
+	aws s3 sync $sync_dir ${s3_bucket}${sync_dir} > /dev/null
+	ul_s=$SECONDS
+	ul_r=$(echo $dir_size_mb $ul_s | awk '{print $1/$2}' )
+	#echo Serial upload took $ul_s s at $ul_r MB/s
 
-#=============================================
-#echo Upload objects in parallel with AWS cp
-#=============================================
-SECONDS=1
-for file in $dir_file_list
-do
-    bn=$(basename ${file})
-    aws s3 cp ${file} ${s3_bucket}${sync_dir}.parallel/${bn} > /dev/null &
-done
-wait
-ul_s=$SECONDS
-ul_r=$(echo $dir_size_mb $ul_s | awk '{print $1/$2}' )
-echo Parallel upload took $ul_s s at $ul_r MB/s
+	#=============================================
+	#echo Serial copy data from bucket with AWS sync
+	#=============================================
+	SECONDS=1
+	aws s3 sync ${s3_bucket}${sync_dir} ${sync_dir}.serial > /dev/null
+	dl_s=$SECONDS
+	dl_r=$(echo $dir_size_mb $dl_s | awk '{print $1/$2}' )
+	#echo Serial download took $dl_s s at $dl_r MB/s
+	
+	#=============================================
+	#echo Clean up local and bucket data
+	#=============================================
+	SECONDS=1
+	aws s3 rm --recursive ${s3_bucket}${sync_dir} > /dev/null
+	rm_s=$SECONDS
+	rm_r=$(echo $dir_size_mb $rm_s | awk '{print $1/$2}' )
+	#echo Bucket object delete took $rm_s s at $rm_r MB/s
 
-#=============================================
-#echo Rename local files for comparison
-#=============================================
-#mv ${sync_dir} ${sync_dir}.original
+else
 
-#=============================================
-#echo Serial copy data from bucket with AWS sync
-#=============================================
-SECONDS=1
-aws s3 sync ${s3_bucket}${sync_dir} ${sync_dir}.serial > /dev/null
-dl_s=$SECONDS
-dl_r=$(echo $dir_size_mb $dl_s | awk '{print $1/$2}' )
-echo Serial download took $dl_s s at $dl_r MB/s
+	#=============================================
+	#echo Upload objects in parallel with AWS cp
+	#=============================================
+	SECONDS=1
+	for file in $dir_file_list
+	do
+    		bn=$(basename ${file})
+    		aws s3 cp ${file} ${s3_bucket}${sync_dir}/${bn} > /dev/null &
+	done
+	wait
+	ul_s=$SECONDS
+	ul_r=$(echo $dir_size_mb $ul_s | awk '{print $1/$2}' )
+	#echo Parallel upload took $ul_s s at $ul_r MB/s
+	
+	#=============================================
+	#echo Parallel copy data from bucket with AWS cp
+	#=============================================
+	SECONDS=1
+	for file in $dir_file_list
+	do
+    		bn=$(basename $file)
+    		aws s3 cp ${s3_bucket}${file} ${sync_dir}.parallel/${bn} > /dev/null &
+	done
+	wait
+	dl_s=$SECONDS
+	dl_r=$(echo $dir_size_mb $dl_s | awk '{print $1/$2}' )
+	#echo Parallel download took $dl_s s at $dl_r MB/s 
+	
+	#=============================================
+	#echo Clean up local and bucket data
+	#=============================================
+	SECONDS=1
+	aws s3 rm --recursive ${s3_bucket}${sync_dir} > /dev/null
+	rm_s=$SECONDS
+	rm_r=$(echo $dir_size_mb $rm_s | awk '{print $1/$2}' )
+	#echo Bucket object delete took $rm_s s at $rm_r MB/s
 
-#=============================================
-#echo Parallel copy data from bucket with AWS cp
-#=============================================
-SECONDS=1
-for file in $dir_file_list
-do
-    bn=$(basename $file)
-    aws s3 cp ${s3_bucket}${file} ${sync_dir}.parallel/${bn} > /dev/null &
-done
-wait
-dl_s=$SECONDS
-dl_r=$(echo $dir_size_mb $dl_s | awk '{print $1/$2}' )
-echo Parallel download took $dl_s s at $dl_r MB/s 
-
-#=============================================
-#echo Clean up local and bucket data
-#=============================================
-SECONDS=1
-aws s3 rm ${s3_bucket}${sync_dir}
-rm_s=$SECONDS
-rm_r=$(echo $dir_size_mb $rm_s | awk '{print $1/$2}' )
-echo Bucket object delete took $rm_s s at $rm_r MB/s
-
-aws s3 rm ${s3_bucket}${sync_dir}.parallel
-
-#rm -rf ${sync_dir}.original
-#rm -rf ${sync_dir}.parallel
-#rm -rf ${sync_dir}.serial
+fi
 
 #=============================================
 #echo Done
 #=============================================
+rm -rf ${sync_dir}*
+echo $aws_sync $num_files $file_size $size_unit SZ_MB $dir_size_MB UL_S_R $ul_s $ul_r DL_S_R $dl_s $dl_r RM_S_R $rm_s $rm_r
 
